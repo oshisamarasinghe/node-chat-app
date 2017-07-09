37943025932604eee5da6c251bb5f02f2bee0ec5
oshadi <oshadhi.15@cse.mrt.ac.lk>
2017-07-09 10:02:44 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,14 @@
+//console.log(__dirname+'/../public');
+const path=require('path');
+const express=require('express');
+
+const port=process.env.PORT || 3000;
+const publicPath =path.join(__dirname,'../public');
+
+var app=express();
+
+app.use(express.static(publicPath));
+
+app.listen(3000,()=>{
+  console.log('server is up on port',port);
+});
